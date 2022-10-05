c83012d5f9c574a3cf192c42649f77b1cf838abf
Alina <agcarpio@espol.edu.ec>
2022-10-04 23:19:41 -0500

    git commit-m Checked and unchecked option to cross out the label fix

--- main.js
+++ main.js
@@ -10,21 +10,31 @@ todoBoxInput.addEventListener("keypress", e => {
         e.preventDefault();
 
 
-
         const li = document.createElement("li");
         const divcheckbox = document.createElement("div");
         divcheckbox.className = "btn-check";
         const labl = document.createElement("label");
         labl.textContent = text;
 
-        divcheckbox.appendChild(addCheckbox());
+        const buttoncheck = addCheckbox();
+
+
+        divcheckbox.appendChild(buttoncheck);
+
         li.appendChild(divcheckbox);
         li.appendChild(labl);
         li.appendChild(addDeleteIcon());
 
         ulist.appendChild(li);
 
-
+        buttoncheck
+            .onclick = function () {
+            if (!buttoncheck.checked) {
+                labl.style.textDecoration = "none";
+            } else {
+                labl.style.textDecoration = "line-through";
+            }
+        }
         todoBoxInput.value = '';
     }
 
@@ -48,10 +58,18 @@ function addCheckbox() {
     const checkbox = document.createElement("input");
     checkbox.type = 'checkbox';
     checkbox.className = "checkbox-style";
-
-    console.log(checkbox.spellcheck);
     return checkbox;
 }
+
+
+// function addFilters() {
+//     const divFilters = document.createElement("div");
+//     divFilters.className = "filters";
+//     const pItemsLeft = document.createElement("p");
+//     pItemsLeft.textContent
+//
+// }
+
 
 
 
